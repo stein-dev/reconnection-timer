78fd1a9c998e82d3a021c55b4f9aa6f44f539af0
pigscanflyyyy <shinra712@gmail.com>
2021-01-14 00:22:00 +0800

    Add systemctl commands

--- uninstall-reconn.sh
+++ uninstall-reconn.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 # Check if root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -8,7 +10,13 @@ fi
 
 echo 'Created by @pigscanfly | Version 0.0.1'
 
-echo "Uninstalling in progress..."
+echo "Removing files..."
+
+systemctl disable reconn
+
 rm -rf /usr/local/bin/reconn
 rm -rf /etc/systemd/system/reconn.service
-echo "Done"
+
+systemctl daemon-reload
+
+echo "Done. Reboot is recommended. Type reboot to restart vps."

afd7dcb96812c5983687b88bf9d06bdacd0023a8
pigscanflyyyy <shinra712@gmail.com>
2021-01-14 00:27:34 +0800

    Add systemctl commands

--- uninstall-ohp-privoxy.sh
+++ uninstall-ohp-privoxy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+clear
+
 # Check if root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
@@ -8,13 +10,17 @@ fi
 
 echo 'Created by @pigscanfly | Version 0.0.1'
 
-echo "Uninstalling in progress..."
+echo "Removing files..."
 
 DEBIAN_FRONTEND=noninteractive apt purge -y privoxy
+
+systemctl disable ohpserver-ssh
 
 rm -rf /usr/local/bin/ohpserver-ssh
 rm -rf /etc/systemd/system/ohpserver-ssh.service
 rm -rf /etc/privoxy/user.action
 rm -rf /etc/privoxy
 
-echo "Done"
+systemctl daemon-reload
+
+echo "Done. Reboot is recommended. Type reboot to restart vps."
